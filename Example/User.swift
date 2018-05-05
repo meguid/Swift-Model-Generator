0ce0a6576135817db2733ccacc9ab9ad93e984fb
Ahmed Meguid <ameguid12@gmail.com>
2018-05-05 14:54:14 +0200

    Add Example

---
+++ Example/User.swift
@@ -0,0 +1,37 @@
+import Foundation
+import UIKit 
+import ObjectMapper
+
+final class User : NSObject, Mappable {
+
+	var username : String?
+	var age : Int?
+
+	override init() { 
+
+	}
+
+	public required init?(map : Map) {
+
+	}
+
+	func mapping(map : Map) {
+
+		username <- map["username"]
+		age <- map["age"]
+
+	}
+
+	func initWithRealm(realm : UserRealm {
+
+		username = realm.username
+		age = realm.age
+
+	}
+
+	func mapToRealm() -> UserRealm{
+
+		return RealmUser(value : [ "username" : self.username!,"age" : self.age! ])
+
+	}
+}
---
+++ Example/UserRealm.swift
@@ -0,0 +1,9 @@
+import Foundation
+import RealmSwift
+
+class UserRealm : Object {
+
+	dynamic var username = String()
+	dynamic var age = Int()
+
+}
